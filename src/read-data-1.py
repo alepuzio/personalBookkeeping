7473593931d3108a94f19e784922120268d428e0
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2023-02-26 17:55:47 +0100

    rimossi file inutili

--- src/read-data-1.py
+++ src/read-data-1.py
@@ -8,52 +8,22 @@ import clean_data
 class Main:
 
     if __name__ == '__main__':
-        args = InputParameters()
-        #run pyspark
-        spark = SparkFactory.instance( args.job() ) 
-        #run transformation
-        clean_data.run(spark, args.input(), args.output() )
-        spark.stop()
-
-
-class SparkFactory:
-
-    def __init__ (self, newName):
-        self.name = newName
-
-    def instance(self):
-        return  SparkSession.builder.appName(self.name).getOrCreate()
-
-
-class InputParameters:
-
-    """[Summary]
-    :param sys.argv: console input
-    :type sys.argv [ParamType]
-    :raises Error: the number of arguments is less than 3
-    :return: constructor
-    :rtype: InputParameters
-    """
-    def __init__(self, sys):
         arguments = sys.argv
         if(arguments < 3) :
             raise ValueError("The parameters have to be 3: name of the job, input path and output path")
-        self.name = arguments[1]
-        self.dataset_input_path = arguments[2]
-        self.dataset_output_path = arguments[3]
+        job_name = arguments[1]
+        dataset_input_path = arguments[2]
+        dataset_output_path = arguments[3]
+        #run pyspark
+        spark = SparkSession.builder.appName(self.name).getOrCreate()
+        #run transformation
+        
+        print("-"+dataset_input_path+"--")
+        input_dataframe = spark.read.csv(dataset_input_path, header=True)    
+        cleaned_columns = clean_names(input_dataframe.columns)
+        print("-"+cleaned_columns+"--")
+        spark.stop()
 
-    """[Summary]
-    :param none:
-    :raises none: 
-    :return: [the path of the input]
-    :rtype: [String]
-    """
-    def input(self):
-        return self.dataset_input_path
 
 
-    def output(self):
-        return self.dataset_output_path
 
-    def job(self):
-        return self.name
